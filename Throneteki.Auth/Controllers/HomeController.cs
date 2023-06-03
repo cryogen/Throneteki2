af26b3933d670eebfc74d412fd79ac08f0743f52
Stuart Walsh <stu@ipng.org.uk>
2023-06-03 10:02:22 +0100

    add placeholder index page

---
+++ Throneteki.Auth/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Throneteki.Auth.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
